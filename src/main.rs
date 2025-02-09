b239506f95b804ab0c3586e446c2ab5d525392e9
dfjds <arielsalama722@gmail.com>
2025-02-09 05:38:54 +0000

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+extern crate rand;  // imports external crate
+
+use std::io;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+    println!("Guess a number between 1 and 100: ");
+    let secret_num = rand::rng().random_range(1..101);  // Generaye random int within range
+    let mut tries = 0;
+
+    loop {  // infinite loop untill completion
+        let mut input = String::new();  // creats mutable var as empty growable string
+        io::stdin().read_line(&mut input).expect("Failed to read line");  // Reads line
+        
+        // Trims whitspace. Parses and converts to int. Expects non integer results
+        let guess: u32 = input.trim().parse().expect("Invalid input\nPlease type in a number");
+
+        if guess > secret_num {
+            println!("Lower");
+            tries += 1;
+        }
+        else if guess < secret_num {
+            println!("Higher");
+            tries += 1;
+        }
+        else {
+            println!("You got it!!");
+            tries += 1;
+            println!("It took you {} tries", tries);
+            break;  // breaks the loop
+        }
+    } 
+}
